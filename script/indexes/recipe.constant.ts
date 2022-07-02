9a5fa334f4d4e0ebe50ca4992be9ebac0751f2dd
Aiden <aiden@boostcommerce.net>
2022-07-02 15:11:33 +0700

    update recipe index mapping

--- script/indexes/recipe.constant.ts
+++ script/indexes/recipe.constant.ts
@@ -8,14 +8,131 @@ export const recipeIndexMapping = {
         id: {
           type: 'keyword',
         },
-        userId: {
+        author: {
+          properties: {
+            id: {
+              type: 'keyword',
+            },
+            name: {
+              type: 'text',
+            },
+            address: {
+              type: 'text',
+            },
+            created_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+            updated_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+          },
+        },
+        title: {
+          type: 'text',
+        },
+        thumbnail: {
           type: 'keyword',
         },
-        createdAt: {
+        content: {
+          type: 'text',
+        },
+        category: {
+          properties: {
+            id: {
+              type: 'keyword',
+            },
+            name: {
+              type: 'text',
+            },
+            thumbnail: {
+              type: 'keyword',
+            },
+            created_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+            updated_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+          },
+        },
+        tags: {
+          type: 'nested',
+          properties: {
+            id: {
+              type: 'keyword',
+            },
+            name: {
+              type: 'keyword',
+            },
+            type: {
+              type: 'keyword',
+            },
+            created_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+            updated_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+          },
+        },
+        nutrition: {
+          type: 'text',
+        },
+        products: {
+          type: 'nested',
+          properties: {
+            id: {
+              type: 'keyword',
+            },
+            name: {
+              type: 'text',
+            },
+            data_source: {
+              type: 'keyword',
+            },
+            data_source_url: {
+              type: 'keyword',
+            },
+            created_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+            updated_at: {
+              type: 'date',
+              format: 'date_optional_time',
+            },
+          },
+        },
+        views: {
+          type: 'integer',
+          null_value: 0,
+        },
+        search: {
+          properties: {
+            merged_search: {
+              type: 'text',
+              // analyzer: 'icu_w_symbol_analyzer',
+            },
+            tags: {
+              type: 'text',
+              // analyzer: 'icu_w_symbol_analyzer',
+            },
+            products: {
+              type: 'text',
+            },
+          },
+        },
+        created_at: {
           type: 'date',
           format: 'date_optional_time',
         },
-        updatedAt: {
+        updated_at: {
           type: 'date',
           format: 'date_optional_time',
         },
