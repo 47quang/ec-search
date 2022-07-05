5a589df0c849aba63caf3cf2776d5d34f744a928
Aiden <aiden@boostcommerce.net>
2022-07-05 22:47:54 +0700

    update recipe mapping for autocomplete search on tag field

--- script/indexes/recipe.constant.ts
+++ script/indexes/recipe.constant.ts
@@ -1,7 +1,55 @@
 export const recipeIndexMapping = {
   index: 'ec-recipes',
   body: {
-    settings: {},
+    settings: {
+      analysis: {
+        char_filter: {
+          special_mappingv2: {
+            type: 'mapping',
+            mappings: [
+              "' => _pfssq_",
+              '" => _pfsdq_',
+              '@ => _pfsat_',
+              '# => _pfssh_',
+              '$ => _pfsdl_',
+              '% => _pfspc_',
+              '^ => _pfsca_',
+              '& => _pfsan_',
+              '* => _pfsas_',
+              '+ => _pfspl_',
+              ': => _pfsdi_',
+              '/ => _pfssl_',
+              '- => _',
+            ],
+          },
+        },
+        analyzer: {
+          prefix_engram_1_30: {
+            filter: ['lowercase'],
+            type: 'custom',
+            tokenizer: 'edgengram_1_30_tokenizer',
+          },
+          icu_w_symbol_analyzer: {
+            filter: ['lowercase'],
+            char_filter: ['special_mappingv2'],
+            type: 'custom',
+            tokenizer: 'icu_tokenizer',
+          },
+          ngram_search: {
+            filter: ['lowercase'],
+            tokenizer: 'whitespace',
+          },
+        },
+        tokenizer: {
+          edgengram_1_30_tokenizer: {
+            token_chars: ['letter', 'digit', 'punctuation', 'symbol'],
+            min_gram: '1',
+            type: 'edge_ngram',
+            max_gram: '30',
+          },
+        },
+      },
+    },
     mappings: {
       dynamic: 'false',
       properties: {
@@ -66,7 +114,15 @@ export const recipeIndexMapping = {
               type: 'keyword',
             },
             name: {
-              type: 'keyword',
+              type: 'text',
+              fields: {
+                engram: {
+                  type: 'text',
+                  analyzer: 'prefix_engram_1_30',
+                  search_analyzer: 'ngram_search',
+                },
+              },
+              analyzer: 'icu_w_symbol_analyzer',
             },
             type: {
               type: 'keyword',
